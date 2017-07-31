a727f49937c0d05f175b6bcd6271f70bd983e140
Nina <nina.wei@1mifudao.com>
2017-07-31 16:01:00 +0800

    熔断的两种实现方式

--- feign-center/src/main/java/com/loto/springcloud/feigncenter/FeignCenterApplication.java
+++ feign-center/src/main/java/com/loto/springcloud/feigncenter/FeignCenterApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableHystrix
 public class FeignCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeignCenterApplication.class, args);
--- feign-center/src/main/java/com/loto/springcloud/feigncenter/controller/FirstTestController.java
+++ feign-center/src/main/java/com/loto/springcloud/feigncenter/controller/FirstTestController.java
@@ -1,6 +1,8 @@
 package com.loto.springcloud.feigncenter.controller;
 
 import com.loto.springcloud.feigncenter.client.FirstTestClient;
+import com.loto.springcloud.feigncenter.client.SecondTestClient;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +14,23 @@ public class FirstTestController {
     @Autowired
     FirstTestClient firstTestClient;
 
+    @Autowired
+    SecondTestClient secondTestClient;
+
     @RequestMapping(value = "/tt/add" ,method = RequestMethod.GET)
     @ResponseBody
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         return firstTestClient.add(a,b);
+    }
+
+    @RequestMapping(value = "/tt/substract" ,method = RequestMethod.GET)
+    @ResponseBody
+    @HystrixCommand(fallbackMethod = "substractErrProcess")
+    public Integer substract(@RequestParam Integer a, @RequestParam Integer b) {
+        return secondTestClient.substract(a,b);
+    }
+
+    public Integer substractErrProcess(@RequestParam Integer a, @RequestParam Integer b) {
+        return -99;
     }
 }
